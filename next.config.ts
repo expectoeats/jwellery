183b622f75a17e9321af658041c80e65f637c122
expectoeats <expectoeats@gmail.com>
2026-09-14 01:53:57 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media.angara.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.caratlane.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+
